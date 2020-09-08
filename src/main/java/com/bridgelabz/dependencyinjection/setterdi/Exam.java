0e264bdc5a07e093861e746a43d1422509e71e6e
kalyaniRane <kalyanirane19@gmail.com>
2020-09-08 18:31:38 +0530

    ADDED : Basic Spring Program To Introduce A DependencyInjection by Setter

---
+++ src/main/java/com/bridgelabz/dependencyinjection/setterdi/Exam.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.dependencyinjection.setterdi;
+
+public class Exam {
+    public static void main(String[] args) {
+        Student student=new Student();
+        student.studentName="kalyani";
+
+        student.displayStudentInfo();
+    }
+}
---
+++ src/main/java/com/bridgelabz/dependencyinjection/setterdi/Student.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.dependencyinjection.setterdi;
+
+public class Student {
+
+    String studentName;
+
+    public void displayStudentInfo(){
+        System.out.println("Student name: "+studentName);
+    }
+
+}
