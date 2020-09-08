d1ca523be87b685282f2eabd56e5156d7aee5ab9
kalyaniRane <kalyanirane19@gmail.com>
2020-09-08 17:04:20 +0530

    ADDED : GetBean Method To Access The Bean To Call Methods Of Class

--- src/main/java/com/bridgelabz/ioc/javaclass/Mobile.java
+++ src/main/java/com/bridgelabz/ioc/javaclass/Mobile.java
@@ -9,6 +9,11 @@ public class Mobile {
     public static void main(String[] args) {
         ApplicationContext context =new ClassPathXmlApplicationContext("beans.xml");
         System.out.println("context is loaded");
+
+        Airtel air = (Airtel) context.getBean("airtel");
+        air.calling();
+        air.data();
+
     }
 
 }
