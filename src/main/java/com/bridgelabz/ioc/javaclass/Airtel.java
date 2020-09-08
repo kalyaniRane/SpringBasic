90d9841045d46432a048075b02ad9a5642d25a4a
kalyaniRane <kalyanirane19@gmail.com>
2020-09-08 16:46:51 +0530

    ADDED : Constructor To Check Airtel Object Is Called By Container Or Not

--- src/main/java/com/bridgelabz/ioc/javaclass/Airtel.java
+++ src/main/java/com/bridgelabz/ioc/javaclass/Airtel.java
@@ -3,6 +3,11 @@ package com.bridgelabz.ioc.javaclass;
 import com.bridgelabz.ioc.interfaces.Sim;
 
 public class Airtel implements Sim {
+
+    public Airtel() {
+        System.out.println("Called airtel");
+    }
+
     @Override
     public void calling() {
         System.out.println("Calling from Airtel sim");
